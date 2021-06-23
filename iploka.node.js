3a62fa3b93be2009b6433cb76cb3a4c1ed5447b3
Donald Ng <donaldyann@gmail.com>
2021-06-23 15:33:10 +0800

    Improvement on formatting
    Add support to response in both callback and promise

--- iploka.node.js
+++ iploka.node.js
@@ -1,63 +1,67 @@
-
 'use strict';
 
-var https = require('https');
+const https = require('https');
 
-var API_KEY = '';
+module.exports = class {
+  constructor(key) {
+    this.apikey = key
+    this.headers = {'user-agent': 'iploka/iploka-nodejs'};
+  }
 
-var headers   = {'user-agent': 'iploka/iploka-nodejs'};
-
-var _request = function(path, callback, isJson){
-    var options = {
+  httpReq (path) {
+    return new Promise((resolve) => {
+      var options = {
         host: 'api.iploka.com',
         path: path,
-        headers: headers
-    };
+        headers: this.headers
+      };
 
-    var req = https.get(options, function(resp){
+      var req = https.get(options, function(resp){
         var body = ''
 
         resp.on('data', function(data){
-            body += data;
+          body += data;
         });
 
         resp.on('end', function(){
-            if (isJson) {
-                var loc = JSON.parse(body);            
-                callback(loc);
-            } else {
-                var loc = body;
-                callback(loc);
-            }
+          var loc = body;
+          resolve(loc);
         });
-    });
+      });
 
-    req.on('error', function(e) {
-      callback(new Error(e));
-    });
-};
+      req.on('error', function(e) {
+        resolve(new Error(e));
+      });
+    })
+  }
 
-var location = function(callback, ip, key){
-    var path;
+  lookup (ip, callback) {
+    return new Promise((resolve, reject) => {
+      let path = '/' + ip + '?api_key=' + this.apikey;
+      this.httpReq(path)
+      .then(result => {
+        if (typeof callback === 'function') callback(null, result)
+        resolve(result)
+      })
+      .catch(err => {
+        if (typeof callback === 'function') callback(err, null)
+        reject(err)
+      })
+    })
+  }
 
-    if (typeof callback !== 'function') {
-        return 'Callback function is required';
-    }
-
-    if ((typeof key !== 'undefined') && (key !== '')){
-        path = '/' + ip + '?api_key='+ key;
-    } else {
-        if (API_KEY !== ''){
-            path = '/' + ip + '?api_key='+ API_KEY;
-        } 
-    }
-    _request(path, callback, (!isField))
-};
-
-
-/**
- * Query location for an IP address
- */
-module.exports = {
-    location : location,
-};
+  check (callback) {
+    return new Promise((resolve, reject) => {
+      var path = '/check?api_key=' + this.apikey;
+      this.httpReq(path)
+      .then(result => {
+        if (typeof callback === 'function') callback(null, result)
+        resolve(result)
+      })
+      .catch(err => {
+        if (typeof callback === 'function') callback(err, null)
+        reject(err)
+      })
+    })
+  }
+}
