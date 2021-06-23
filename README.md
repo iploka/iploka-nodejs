70d8eda9b87d3ebc0ce8f387082e60c0abe6780a
Allen <allendbradley97@gmail.com>
2021-06-23 09:26:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+## Installation
+```
+npm install iploka.nodejs --save
+```
+
+## Usage 
+```
+var ipLoka = require('iploka.nodejs');
+
+var callback = function(res){
+    console.log(res);
+};
+
+ipLoka.location(callback, 'ip', 'secret-key')  // Complete location for your IP address
+
+API key can be specified in the following ways : 
+
+1. Inside `iploka.js` by initializing the value of `API_KEY` variable
+2. As a function argument e.g. `iploka.location(callback, '8.8.8.8', 'secret-key')`
+
+```
+## Get your free API Key at https://iploka.com
+
---
+++ iploka-node.js
@@ -0,0 +1,64 @@
+
+'use strict';
+
+var https = require('https');
+
+var API_KEY = '';
+
+var headers   = {'user-agent': 'iploka/iploka-nodejs'};
+
+var _request = function(path, callback, isJson){
+    var options = {
+        host: 'api.iploka.com',
+        path: path,
+        headers: headers
+    };
+
+    var req = https.get(options, function(resp){
+        var body = ''
+
+        resp.on('data', function(data){
+            body += data;
+        });
+
+        resp.on('end', function(){
+            if (isJson) {
+                var loc = JSON.parse(body);            
+                callback(loc);
+            } else {
+                var loc = body;
+                callback(loc);
+            }
+        });
+    });
+
+    req.on('error', function(e) {
+      callback(new Error(e));
+    });
+};
+
+
+var location = function(callback, ip, key){
+    var path;
+
+    if (typeof callback !== 'function') {
+        return 'Callback function is required';
+    }
+
+    if ((typeof key !== 'undefined') && (key !== '')){
+        path = '/' + ip + '?api_key='+ key;
+    } else {
+        if (API_KEY !== ''){
+            path = '/' + ip + '?api_key='+ API_KEY;
+        } 
+    }
+    _request(path, callback, (!isField))
+};
+
+
+/**
+ * Query location for an IP address
+ */
+module.exports = {
+    location : location,
+};
